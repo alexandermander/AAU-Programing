0d3ab52231eb3809debc6bae4b3d4dfbb9d9b036
Alexander Sumczynski <alexanderxboxone@gmail.com>
2022-11-20 11:20:51 +0100

    some fulders

---
+++ Clion_pro/yatzi.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+
+
+void start_game();
+int roll_multiple_dies();
+
+
+
+
+int main(void )
+{
+    start_game();
+    return 0;
+
+
+
+}
+
+
+void start_game(){
+    roll_multiple_dies();
+
+}
+
+
+int roll_multiple_dies(){
+    return 0;
+}
+
