5a68482d22f06a2edda1a11cd1cf9c45aa6b4745
Alexander <alexandermander123@gmail.com>
2022-11-27 21:17:12 +0100

    jeg har den nu

--- Clion_pro/fodboldturninging.c
+++ Clion_pro/fodboldturninging.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define LENTHOFTXT 102
 
 typedef struct{
@@ -8,21 +9,59 @@ typedef struct{
     double time;
     char team_one[4];
     char team_two[4];
-    int onedues;
-}match;
+    int soceone,socetwo,audinc;
+}MATCH;
+
+typedef struct{
+    char teamName[4];
+    int score_aginet[11];
+}soccer_team;
+
 
 
 int main(void){
-    match matches[102];
-    FILE *file_file = fopen("kampe-2022-2023.txt", "r");
-    if (file_file == NULL){
+    MATCH matches[102];
+    FILE *fin = fopen("kampe-2022-2023.txt", "r");
+    if (fin == NULL){
         exit(EXIT_FAILURE);
     }
     int i = 0;
-    while (!feof(file_file)){
-        fscanf(file_file, "%s %s %lf %s %s %d", matches[i].weekday, matches[i].date, matches[i].time, matches[i].team_one, matches[i].team_two, matches[i].onedues);
+    while (!feof(fin)){
+        fscanf(fin, "%s %s %lf %s - %s %d - %d %d",
+               matches[i].weekday,
+               matches[i].date,
+               &matches[i].time,
+               matches[i].team_one,
+               matches[i].team_two,
+               &matches[i].soceone,
+               &matches[i].socetwo,
+               &matches[i].audinc);
+
+
+
+
+
         i++;
     }
+    fclose(fin);
+    soccer_team teams[12] = {0};
 
 
+
+    int k = 0;
+    for (int j = 0; j < LENTHOFTXT; ++j) {
+        matches[j].team_one;
+        for (k; k < 12; ++k) {
+            if (matches[j].team_one != teams[k].teamName){
+                strncpy(teams[k].teamName, matches[j].team_one, 4);
+                break;
+            }
+        }
+    }
+    for (int j = 0; j < 12; ++j) {
+        printf("%s", matches[j].team_one);
+        printf("\n");
+    }
+    printf("det virerike");
+    return 0;
 }
