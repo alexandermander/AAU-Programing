ede3f8793b7138704521ccba6d93d5de0b4e820a
Alexander <alexandermander123@gmail.com>
2023-07-13 11:03:11 +0200

    tset

---
+++ nvim/lua/alexa/remap.lua
@@ -0,0 +1,13 @@
+vim.g.mapleader = " "
+vim.keymap.set("n","<leader>pv",vim.cmd.Ex)
+vim.keymap.set("n","<leader>qt",vim.cmd.q)
+vim.keymap.set("n","<leader>e",vim.cmd.NeoTreeFocusToggle)
+
+
+vim.keymap.set("n","<leader>y","\"+y")
+vim.keymap.set("n","<leader>y","\"+y")
+
+
+
+
+
